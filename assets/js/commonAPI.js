3e1929d8c9b59ec90811f3f7a174a74e27879b91
zjx <2532116766@qq.com>
2021-01-29 15:24:18 +0800

    完成了首页资料渲染功能

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -1,11 +1,21 @@
-// 为全局axios请求设置根路径
-axios.defaults.baseURL  =  'http://api-breakingnews-web.itheima.net'
+//为全局的axios请求设置根路径
+axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
 
-
-// 添加全局的请求拦截器
+// 添加全局请求拦截器
 axios.interceptors.request.use(function(config) {
+    console.log('----发送 axjax请求前', config);
 
-    console.log('----发送ajax请求前');
+    //获取本地存储的token令牌
+    var token = localStorage.getItem('token') || ''
+
+    //在发送请求之前判断是否有/my开头的请求路径
+
+    //方法一:startswhith
+    //方法二:正则表达式/^\/my
+
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
     // 在发送请求之前做些什么
     return config;
 }, function(error) {
@@ -15,11 +25,21 @@ axios.interceptors.request.use(function(config) {
 
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
-    console.log('----接受ajax请求前', response);
+        console.log('----接受 axajx响应前', response);
+        const { message, status } = response.data;
+        //先判断身份验证是否成功
+        if (message == '身份认证失败！' && status == 1) {
+            //清除本地存储
+            localStorage.removeItem('token');
+            //跳转登录页
+            location.href = './login.html'
+        }
 
-    // 对响应数据做点什么
-    return response.data;
-}, function(error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-});
+
+        // 对响应数据做点什么
+        return response.data;
+    },
+    function(error) {
+        // 对响应错误做点什么
+        return Promise.reject(error);
+    });
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function() {
+
+    // 从layui中提取
+    const { layer } = layui
+
+
+
+    //1.获取用户的个人信息
+    function getUserInfo() {
+
+        //发送ajax请求
+        axios.get('/my/userinfo').then(res => {
+            console.log(res);
+            // 校验请求失败
+            if (res.status !== 0) {
+                return layer.meg('获取用户信息失败')
+            }
+
+            const { data } = res
+            // 渲染用户信息
+            const name = data.nickname || data.username
+
+            // 渲染昵称
+            $('.nickname').text(`欢迎 ${name}`).show()
+
+            // 渲染头像
+            if (data.user_pic) {
+                $('.avater').prop('src', data.user_pic).show()
+                $('.text-avater').hide()
+            } else {
+                $('.text-avater').text(name[0].toUpperCase()).show()
+                $('.avater').hide()
+            }
+        })
+
+    }
+    getUserInfo()
+
+    // 点击退出
+    $('#logout').click(function() {
+        // 请求接口
+        // 1.清除token
+        localStorage.removeItem('token')
+
+        // 2.跳转登录页
+        location.href = './login.html'
+    })
+
+})

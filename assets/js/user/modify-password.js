e7f9308962da04edd7d0329353648daafba4b088
zjx <2532116766@qq.com>
2021-01-31 15:01:17 +0800

    提交了个人中心模块

--- assets/js/user/modify-password.js
+++ assets/js/user/modify-password.js
@@ -1,36 +1,42 @@
 /* 修改密码模块 */
-$(function () {
-  // 从 layui 中提取模块
-  const { form, layer } = layui
+$(function() {
+    // 从 layui 中提取模块
+    const { form, layer } = layui
 
-  // 1. 表单校验
-  form.verify({
-    pass: [
-      /^\w{6,12}$/,
-      '密码必须6到12位，且不能出现空格'
-    ],
-    confirmPass: function (val) { // val 当前的表单值
-      if (val !== $('#pass').val()) {
-        return '两次密码输入不一致'
-      }
-    }
-  })
-
-  // 2. 表单提交
-  $('.layui-form').submit(function (e) {
-    e.preventDefault()
-
-    // 发起 ajax 请求
-    axios.post('/my/updatepwd', $(this).serialize())
-         .then(res => {
-            console.log(res)
-            // 校验请求失败  
-            if (res.status !== 0) {
-              return layer.msg('修改密码失败!')
+    // 1. 表单校验
+    form.verify({
+        pass: [
+            /^\w{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        confirmPass: function(val) { // val 当前的表单值
+            if (val !== $('#pass').val()) {
+                return '两次密码输入不一致'
             }
-            // 提示用户
-            layer.msg('修改密码成功!')
-         })
-  })
+        }
+    })
+
+    // 2. 表单提交
+    $('.layui-form').submit(function(e) {
+        e.preventDefault()
+
+        // 发起 ajax 请求
+        axios.post('/my/updatepwd', $(this).serialize())
+            .then(res => {
+                console.log(res)
+                    // 校验请求失败  
+                if (res.status !== 0) {
+                    return layer.msg('修改密码失败!')
+                }
+                // 提示用户
+                layer.msg('修改密码成功!')
+
+                // 跳转登录页
+                window.parent.location.href = '../login.js'
+
+                // 移除token令牌
+                localStorage.removeItem('token')
+            })
+    })
 
 })
